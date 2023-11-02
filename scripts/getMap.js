0a0921d7063e5df0d329a0af79ab3395295f5013
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-11-02 22:39:02 +0530

    Map works!!

--- scripts/getMap.js
+++ scripts/getMap.js
@@ -1,9 +1,22 @@
-var map;
-
-function loadMap()
-{
-    map = new Microsoft.Maps.Map(document.getElementById('mapComponent'), {
-        center: new Microsoft.Maps.Location(20.5937, 78.9629),
-        zoom: 5
-    })
+// Initialize the Bing Maps map.
+function initMap() {
+  var map = new Microsoft.Maps.Map('#map-container');
 }
+
+// Handle the search form submission.
+document.getElementById('submit-button').addEventListener('click', function() {
+  // Get the pick-up and drop-off locations from the text boxes.
+  var pickupLocation = document.getElementById('pickup-location').value;
+  var dropoffLocation = document.getElementById('dropoff-location').value;
+
+  // Create a directions route request.
+  var directionsRequest = new Microsoft.Maps.Directions.DirectionsRequest();
+  directionsRequest.setWaypoints([pickupLocation, dropoffLocation]);
+
+  // Calculate the directions.
+  var directionsManager = new Microsoft.Maps.Directions.DirectionsManager();
+  directionsManager.calculateDirections(directionsRequest, function(response) {
+    // Display the directions on the map.
+    map.addDirections(response.routes[0]);
+  });
+});
